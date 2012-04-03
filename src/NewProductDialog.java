676659bb24a085d2af32c82f407c217d500e66d8
Daniel Schramm <dpschramm@gmail.com>
2012-04-04 11:17:02 +1200

    Add 'Not implemented' message box to all buttons.

--- src/NewProductDialog.java
+++ src/NewProductDialog.java
@@ -1,4 +1,25 @@
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 
-public class NewProductDialog {
-
+/**
+ * Provides the user interface for selecting a new product class to create.
+ * 
+ * This extends JDialog to create a dialog window.
+ * 
+ * My first step will be to create a new window that pops up whenever users
+ * click the new  button.
+ * 
+ * @author Daniel Schramm
+ *
+ */
+public class NewProductDialog extends JDialog {
+	
+	private static String title = "Create new product";
+	
+	public NewProductDialog(JFrame frame) {
+		super(frame, title);
+		
+		
+	}
+	
 }
