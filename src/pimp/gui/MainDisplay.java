4131fa0668329f7cc2a3076a5c555bf42606ca24
Daniel Schramm <dpschramm@gmail.com>
2012-04-04 12:09:55 +1200

    Comment and refacor parts of MainDisplay.

--- src/pimp/gui/MainDisplay.java
+++ src/pimp/gui/MainDisplay.java
@@ -20,6 +20,7 @@ import javax.swing.JScrollPane;
 public class MainDisplay extends JFrame {
 	private JTable productTable;
 	public JScrollPane dynamicPanel; // I don't think this should be public, we need getters/setters.
+	private JPanel mainPanel;
 	
 	// New Product Dialog
 	private NewProductDialog npd = new NewProductDialog(this);
@@ -29,92 +30,39 @@ public class MainDisplay extends JFrame {
 	 * Needs to be passed list of objects from database
 	 */
 	public MainDisplay() {
+		
+		// Frame settings.
 		setResizable(false); // We currently use absolute positioning.
-		setBounds(new Rectangle(0, 0, 800, 550));
+		setBounds(new Rectangle(0, 0, 800, 550));	
 		
-		getContentPane().setBounds(new Rectangle(0, 0, 800, 550));
-		getContentPane().setLayout(null);	
-		
-		JPanel mainPanel = new JPanel();
+		// Main panel.
+		mainPanel = new JPanel();
 		mainPanel.setBounds(0, 0, 786, 516);
-		getContentPane().add(mainPanel);
 		mainPanel.setLayout(null);
-		
-		JPanel tablePanel = new JPanel();
-		tablePanel.setBounds(12, 66, 300, 450);
-		mainPanel.add(tablePanel);
-		
-		/* BUTTONS */
-		
-		// New Product Button
-		JButton btnNewProduct = new JButton("New Product");
-		btnNewProduct.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if (!npd.isVisible()) {
-	               npd.setVisible(true);
-	            }
-			}
-		});
-		btnNewProduct.setBounds(12, 17, 144, 25);
-		mainPanel.add(btnNewProduct);
-		
-		
-		// Load Product Button
-		JButton btnLoadProducts = new JButton("Load Products");
-		btnLoadProducts.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				JOptionPane.showMessageDialog(getContentPane(), 
-						"Not yet implemented.");
-			}
-		});
-		btnLoadProducts.setBounds(168, 17, 144, 25);
-		mainPanel.add(btnLoadProducts);
-		
-		// Edit Product Button
-		JButton btnEdit = new JButton("Edit");
-		btnEdit.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				JOptionPane.showMessageDialog(getContentPane(), 
-						"Not yet implemented.");
-			}
-		});
-		btnEdit.setBounds(363, 17, 117, 25);
-		mainPanel.add(btnEdit);
-		
-		// Copy Product Button
-		JButton btnCopy = new JButton("Copy");
-		btnCopy.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				JOptionPane.showMessageDialog(getContentPane(), 
-						"Not yet implemented.");
-			}
-		});
-		btnCopy.setBounds(492, 17, 117, 25);
-		mainPanel.add(btnCopy);
-		
-		// Delete Product Button
-		JButton btnDelete = new JButton("Delete");
-		btnDelete.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				JOptionPane.showMessageDialog(getContentPane(), 
-						"Not yet implemented.");
-			}
-		});
-		btnDelete.setBounds(621, 17, 117, 25);
-		mainPanel.add(btnDelete);
 		
 		// ??
 		dynamicPanel = new JScrollPane();
 		dynamicPanel.setBounds(343, 66, 416, 450);
 		mainPanel.add(dynamicPanel);
 		
-		/* PRODUCT TABLE */
+		// Button Setup
+		createButtons();
 		
+		// Product Table Setup
+		createProductTable();
+		
+		// Add main panel to frame.
+		getContentPane().setBounds(new Rectangle(0, 0, 800, 550));
+		getContentPane().setLayout(null);
+		getContentPane().add(mainPanel);
+	}
+	
+	/**
+	 * This method creates the product table.
+	 */
+	private void createProductTable() {
+		
+		// Create table.
 		productTable = new JTable();
 		productTable.setModel(new DefaultTableModel(
 			new Object[][] {
@@ -133,18 +81,97 @@ public class MainDisplay extends JFrame {
 				return columnEditables[column];
 			}
 		});
+		
+		// Settings
 		productTable.getColumnModel().getColumn(0).setResizable(false);
 		productTable.getColumnModel().getColumn(0).setPreferredWidth(100);
 		productTable.getColumnModel().getColumn(0).setMinWidth(100);
 		productTable.getColumnModel().getColumn(1).setPreferredWidth(190);
 		productTable.getColumnModel().getColumn(1).setMinWidth(190);
+		
+		// Create table panel
+		JPanel tablePanel = new JPanel();
+		tablePanel.setBounds(12, 66, 300, 450);
 		tablePanel.add(productTable);
+		
+		// Add to main panel.
+		mainPanel.add(tablePanel);
 	}
 	
 	/**
 	 * 
 	 */
-	void populateProductTable(){
+	private void populateProductTable(){
 		
 	}
+	
+	/**
+	 * This method contains all the button setup code.
+	 */
+	private void createButtons() {	
+		
+		// Create New Product Button
+		JButton btnNewProduct = new JButton("New Product");
+		btnNewProduct.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (!npd.isVisible()) {
+	               npd.setVisible(true);
+	            }
+			}
+		});
+		btnNewProduct.setBounds(12, 17, 144, 25);
+		
+		// Create Load Product Button
+		JButton btnLoadProducts = new JButton("Load Products");
+		btnLoadProducts.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(getContentPane(), 
+						"Not yet implemented.");
+			}
+		});
+		btnLoadProducts.setBounds(168, 17, 144, 25);
+		
+		// Create Edit Product Button
+		JButton btnEdit = new JButton("Edit");
+		btnEdit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(getContentPane(), 
+						"Not yet implemented.");
+			}
+		});
+		btnEdit.setBounds(363, 17, 117, 25);
+		
+		// Create Copy Product Button
+		JButton btnCopy = new JButton("Copy");
+		btnCopy.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(getContentPane(), 
+						"Not yet implemented.");
+			}
+		});
+		btnCopy.setBounds(492, 17, 117, 25);
+		
+		// Create Delete Product Button
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(getContentPane(), 
+						"Not yet implemented.");
+			}
+		});
+		btnDelete.setBounds(621, 17, 117, 25);
+		
+		// Add buttons to main panel.
+		mainPanel.add(btnNewProduct);
+		mainPanel.add(btnLoadProducts);
+		mainPanel.add(btnEdit);
+		mainPanel.add(btnCopy);
+		mainPanel.add(btnDelete);
+	}
+
 }
