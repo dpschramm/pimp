b389c9bc4835a94bd4575dbbac25e3b2c53effb4
Simon Flowers <simongflowers@gmail.com>
2012-05-08 16:18:28 +1200

    Added MovieTicket.jar

---
+++ src/pimp/shoe/MovieTicket.java
@@ -0,0 +1,20 @@
+package pimp.shoe;
+
+import java.util.Date;
+
+import pimp.form.FormField;
+import pimp.model.Product;
+
+public class MovieTicket extends Product {
+
+	@FormField(displayName="Time")
+	public Date time;
+	
+	@FormField(displayName="Theatre")
+	public int Theatre;
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
